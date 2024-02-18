5c830289a81d24b05063ef885729b82dba3e34a3
artem-chizhov <113351797+artem-chizhov@users.noreply.github.com>
2024-02-18 13:56:20 +0300

    Add files via upload

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,32 +1,13 @@
 package org.example;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.hc.client5.http.classic.methods.HttpGet;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        String  url = "https://api.nasa.gov/planetary/apod?" +
-                "api_key=JfsUe24z2n4Tk9G84lDTzbsKl9RByB1NKormE9Jo";
-        CloseableHttpClient httpClient = HttpClients.createDefault();
-        ObjectMapper mapper = new ObjectMapper();
+    public static void main(String[] args) throws IOException, TelegramApiException {
 
-        HttpGet request = new HttpGet(url);
-        CloseableHttpResponse response = httpClient.execute(request);
-        Nasa answer = mapper.readValue(response.getEntity().getContent(), Nasa.class);
+        new NasaBot("NasaBot_Java","6515878588:AAHfNisuUeTuS8HVvGTxh45eS_iDlKjJK_g");
 
-        HttpGet imageGet = new HttpGet(answer.hdurl);
-
-        String[] urlSplited = answer.url.split("/");
-        String filename = urlSplited[urlSplited.length -1];
-
-        CloseableHttpResponse image = httpClient.execute(imageGet);
-        FileOutputStream fileOutputStream = new FileOutputStream(filename);
-        image.getEntity().writeTo(fileOutputStream);
     }
 }
